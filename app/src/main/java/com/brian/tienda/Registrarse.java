ad4ae54435416ee6dc9af0870f8d128a3cb1605b
brallan borh blanquiceth mosquera <72157817+bblanqui@users.noreply.github.com>
2021-09-26 20:09:31 -0500

    se agrega validacion de campos correo, contraseña y usuario

--- app/src/main/java/com/brian/tienda/Registrarse.java
+++ app/src/main/java/com/brian/tienda/Registrarse.java
@@ -5,15 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 import com.example.tienda.R;
 
 public class Registrarse extends AppCompatActivity {
-
+    EditText usuario, correo, pass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrarse);
+        usuario = findViewById(R.id.usuario);
+        correo = findViewById(R.id.correo);
+        pass = findViewById(R.id.pass);
     }
 
     public void views(View view) {
@@ -21,4 +25,11 @@ public class Registrarse extends AppCompatActivity {
 
         startActivity(intentdo);
     }
+
+    public  void validaciones (View view){
+
+        Validaciones Validacion = new Validaciones(usuario, correo, pass);
+    }
+
+
 }
