05828b0aaca9e959bcae7c2c6c9676d0c5aa948b
brallan borh blanquiceth mosquera <72157817+bblanqui@users.noreply.github.com>
2021-10-03 16:53:09 -0500

    se agrega validacion de campos correo, contraseña y usuario

--- app/src/main/java/com/brian/tienda/Registrarse.java
+++ app/src/main/java/com/brian/tienda/Registrarse.java
@@ -6,9 +6,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.tienda.R;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Registrarse extends AppCompatActivity {
@@ -31,11 +33,14 @@ public class Registrarse extends AppCompatActivity {
     }
 
     public void validaciones(View view) {
-
+        Pattern p = Pattern.compile("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?");
+        Pattern c = Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$");
 
         String usuarios = usuario.getText().toString();
         String correos = correo.getText().toString();
         String contrasena = pass.getText().toString();
+        Matcher m = p.matcher(correos);
+        Matcher M = c.matcher(contrasena);
 
 
         if (usuarios.isEmpty() || usuarios.length() < 3 || Pattern.compile(" {1,}").matcher(usuario.getText().toString()).find()) {
@@ -46,8 +51,17 @@ public class Registrarse extends AppCompatActivity {
             showError(correo, "correo invalido");
 
 
+        }else if(!m.find()){
+
+            showError(correo, "correo invalido");
+
         } else if (contrasena.isEmpty() || contrasena.length() < 8 || Pattern.compile(" {1,}").matcher(pass.getText().toString()).find())  {
             showError(pass, "contraseña invalida");
+
+        }else if (!M.find()){
+
+            Toast.makeText(this, "almenos un dijito, un caracter espacial @ y sin espacios", Toast.LENGTH_LONG);
+            showError(pass, "contrasena invalida");
 
         }else {
 
--- app/src/main/java/com/brian/tienda/login.java
+++ app/src/main/java/com/brian/tienda/login.java
@@ -5,22 +5,28 @@ import com.example.tienda.R;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class login extends AppCompatActivity implements Iinten{
     EditText correolog, contralog;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+
         correolog= findViewById(R.id.correolog);
         contralog= findViewById(R.id.contralog);
         correolog.requestFocus();
+
+
     }
 
     public void views(View view) {
@@ -30,21 +36,42 @@ public class login extends AppCompatActivity implements Iinten{
     }
 
     public void  validators (View view){
-
+            Pattern p = Pattern.compile("[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?");
+            Pattern c = Pattern.compile("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$");
             String correo = correolog.getText().toString();
             String contrasena = contralog.getText().toString();
+            Matcher m = p.matcher(correo);
+            Matcher M = c.matcher(contrasena);
+                if( correo.isEmpty()){
+
+                showError(correolog, "correo no puede ir vacio");
+
+            }else if(!m.find()){
+
+                    showError(correolog, "correo invalido");
+
+                }
 
 
-            if(correo.isEmpty()  || !correo.contains("@") || Pattern.compile(" {1,}").matcher(correolog.getText().toString()).find() || correolog.length() < 3){
 
-                showError(correolog, "correo invalido");
-
-            }else if(contrasena.isEmpty() || contrasena.length() < 8 || Pattern.compile(" {1,}").matcher(contralog.getText().toString()).find() ){
-
-                showError(contralog, "contrasena invalida");
+                else if(contrasena.isEmpty() || contrasena.length() < 8 || Pattern.compile(" {1,}").matcher(contralog.getText().toString()).find() ){
 
 
-            }else {
+                    showError(contralog, "contrasena invalida");
+                    Toast.makeText(this, "almenos un dijito, un caracter espacial @ y sin espacios", Toast.LENGTH_LONG);
+
+
+            }else if (!M.find()){
+
+                    Toast.makeText(this, "almenos un dijito, un caracter espacial @ y sin espacios", Toast.LENGTH_LONG);
+                    showError(contralog, "contrasena invalida");
+
+                }
+
+
+
+
+                else {
 
                 Intent intentdo = new Intent(this, inicio.class);
 
