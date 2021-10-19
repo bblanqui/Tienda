00f4bc6ce845c1eb0fe0c059319fa7b8122fe8c0
brallan borh blanquiceth mosquera <72157817+bblanqui@users.noreply.github.com>
2021-10-19 17:31:29 -0500

    auth complete

--- app/src/main/java/com/brian/tienda/Registrarse.java
+++ app/src/main/java/com/brian/tienda/Registrarse.java
@@ -16,12 +16,18 @@ import android.widget.Toast;
 import com.example.tienda.R;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.FirebaseDatabase;
+//import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -31,7 +37,8 @@ public class Registrarse extends AppCompatActivity {
     EditText usuario, correo, pass;
     Spinner roles;
     private FirebaseAuth mAuth;
-
+    FirebaseFirestore db;
+    Map<String, String> user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,7 +53,8 @@ public class Registrarse extends AppCompatActivity {
 
         ArrayAdapter<CharSequence> adapter=ArrayAdapter.createFromResource(this, R.array.rol, android.R.layout.simple_spinner_item);
         roles.setAdapter(adapter);
-
+       db = FirebaseFirestore.getInstance();
+        user = new HashMap<>();
 
     }
 
@@ -98,7 +106,23 @@ public class Registrarse extends AppCompatActivity {
                                 // Sign in success, update UI with the signed-in user's information
                                 Toast.makeText(Registrarse.this, "Authentication ok.",
                                         Toast.LENGTH_SHORT).show();
-                                FirebaseUser user = mAuth.getCurrentUser();
+                                user.put("usuario", usuarios);
+                                user.put("correo", correos);
+                                user.put("rol", roles.getAutofillValue().toString());
+                               db.collection("dbcomputec")
+                                        .add(user)
+                                        .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                            @Override
+                                            public void onSuccess(DocumentReference documentReference) {
+                                                Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                                            }
+                                        })
+                                        .addOnFailureListener(new OnFailureListener() {
+                                            @Override
+                                            public void onFailure(@NonNull Exception e) {
+                                                Log.w(TAG, "Error adding document", e);
+                                            }
+                                        });
 
                             } else {
                                 // If sign in fails, display a message to the user.
