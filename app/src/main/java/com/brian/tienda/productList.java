32a3ca656967045fbe37c2760c399fbe7586516a
defaultUser <71094683+Pablo-pelaez@users.noreply.github.com>
2021-10-21 20:29:11 -0500

    SE AÑADE BINDING2

--- app/src/main/java/com/brian/tienda/productList.java
+++ app/src/main/java/com/brian/tienda/productList.java
@@ -6,11 +6,17 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.brian.tienda.entities.ProductEntity;
 import com.example.tienda.R;
 import com.example.tienda.databinding.ActivityProductListBinding;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class productList extends AppCompatActivity {
     private ActivityProductListBinding productlistbinding;
+
+    List<ProductEntity> product = new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
